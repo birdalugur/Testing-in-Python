6f46701a041d92c136e0a405d703679176428c5a
birdalugureren <birdalugureren@gmail.com>
2020-06-23 23:04:25 +0300

    handle_errors

--- test_my_sum.py
+++ test_my_sum.py
@@ -8,9 +8,15 @@ class TestSum(unittest.TestCase):
         """
         Test that it can sum a list of integers
         """
-        data = [1,2,3]
+        data = [1, 2, 3]
         result = mysum(data)
-        self.assertEqual(result,6)
+        self.assertEqual(result, 6)
+
+    def test_bad_type(self):
+        data = "banana"
+        with self.assertRaises(TypeError):
+            result = sum(data)    
+
 
 if __name__ == '__main__':
     unittest.main()
